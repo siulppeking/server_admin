a91baf5ea22f4f3e96a94d03729118859aba2b35
siulppeking <elchancateclas@gmail.com>
2025-10-03 06:59:32 -0500

    first commit

---
+++ src/test/java/pe/mil/fap/ServerAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.mil.fap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
